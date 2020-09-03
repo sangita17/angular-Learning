22e859929af37d435593c0bb199d673ec8277cb3
sangita17 <sangitachauhan1701@gmail.com>
2020-09-03 18:59:57 +0900

    4 files modified

--- src/app/web-basics/customPipe.ts
+++ src/app/web-basics/customPipe.ts
@@ -8,7 +8,7 @@ export class customOrderByPipe implements PipeTransform {
     if (field) items.sort((a, b) => a[field] > b[field] ? 1 : -1);
     else items.sort((a, b) => a > b ? 1 : -1);
 
-    if (reverse) items.reverse();
+    //if (reverse) items.reverse();
 
     return items;
   }
--- src/app/web-basics/web-basics.component.ts
+++ src/app/web-basics/web-basics.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {WebCallComponent} from '../web-call/web-call.component'
 import {WebCallService} from '../web-call/web-call.service'
-import {customOrderByPipe} from '../web-basics/customPipe'
+import {customOrderByPipe} from './customPipe'
+
 @Component({
   selector: 'app-web-basics',
   templateUrl: './web-basics.component.html',
@@ -11,6 +12,14 @@ export class WebBasicsComponent implements OnInit {
 webgallery;
 menuItems;
 bithday= '1990/01/17';
+order = "title";
+ 
+restaurantLists = [
+    { name: "Himalya Tempura", address: "9-1-3, Sishin-cho, Edogawa-Ku", price: 3244 },
+    { name: "Bikora Tempura", address: "1-1-3, Mishin-cho, Edogawa-Ku", price: 2244},
+    {name: "Lohama Tempura", address: "5-1-3, Tishin-cho, Edogawa-Ku", price: 4244 },
+  ];
+
   constructor(private webService : WebCallService) { }
 
   ngOnInit() {
